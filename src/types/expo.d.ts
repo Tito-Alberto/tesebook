8f5c7106d53599ba09c30ffdf4117b78a4206a55
unknown <alberepalanga@gmail.com>
2026-02-04 07:31:57 +0100

    docs: comenta tipos expo

--- src/types/expo.d.ts
+++ src/types/expo.d.ts
@@ -1,7 +1,9 @@
+// Tipos basicos para expo-asset
 declare module 'expo-asset' {
   export const Asset: any;
 }
 
+// Tipos basicos para expo-font
 declare module 'expo-font' {
   const Font: any;
   export = Font;
